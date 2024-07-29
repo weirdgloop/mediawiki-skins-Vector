a45c88c59f3afb8cf450935329361484138ab6c2
Jayden Bailey <jaydenkieran@gmail.com>
2024-07-29 22:47:53 +0100

    First pass at a MVP for adding Vector22 search to LegacyVector

--- resources/skins.vector.legacy.js/skin-legacy.js
+++ resources/skins.vector.legacy.js/skin-legacy.js
@@ -6,12 +6,14 @@ const
 	pageReady = require( /** @type {string} */( 'mediawiki.page.ready' ) ),
 	portlets = require( './portlets.js' ),
 	vector = require( './vector.js' ),
+	initSearchLoader = require( './searchLoader.js' ).initSearchLoader,
 	teleportTarget = /** @type {HTMLElement} */require( /** @type {string} */ ( 'mediawiki.page.ready' ) ).teleportTarget;
 
 function main() {
 	collapsibleTabs.init();
 	$( vector.init );
 	portlets.main();
+	initSearchLoader( document );
 	pageReady.loadSearchModule( 'mediawiki.searchSuggest' );
 	teleportTarget.classList.add( 'vector-body' );
 }
