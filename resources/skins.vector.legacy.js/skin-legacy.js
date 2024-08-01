70a189e3fa5b3906b0eb361f291b265aa0bd6006
Jayden Bailey <jaydenkieran@gmail.com>
2024-08-01 20:25:19 +0100

    Search: do not load mediawiki.searchSuggest anymore

--- resources/skins.vector.legacy.js/skin-legacy.js
+++ resources/skins.vector.legacy.js/skin-legacy.js
@@ -2,8 +2,6 @@
 /** @interface MediaWikiPageReadyModule */
 const
 	collapsibleTabs = require( './collapsibleTabs.js' ),
-	/** @type {MediaWikiPageReadyModule} */
-	pageReady = require( /** @type {string} */( 'mediawiki.page.ready' ) ),
 	portlets = require( './portlets.js' ),
 	vector = require( './vector.js' ),
 	teleportTarget = /** @type {HTMLElement} */require( /** @type {string} */ ( 'mediawiki.page.ready' ) ).teleportTarget;
@@ -12,7 +10,6 @@ function main() {
 	collapsibleTabs.init();
 	$( vector.init );
 	portlets.main();
-	pageReady.loadSearchModule( 'mediawiki.searchSuggest' );
 	teleportTarget.classList.add( 'vector-body' );
 }
 
